916ad4c4e490aabcd724ec5ff9188776b7a44c44
4tuna <4tuna.uno@gmail.com>
2023-08-15 05:26:10 +0900

    update: 最後にアクティブになった動画再生ウィンドウのみ操作するように

--- background.js
+++ background.js
@@ -1,10 +1,43 @@
 'use strict'
 
-chrome.commands.onCommand.addListener(async (command) => {
+let lastActiveTab = null
+const targetUrl = 'https://animestore.docomo.ne.jp/animestore/sc_d_pc?.*'
+
+chrome.windows.onCreated.addListener(async () => {
+    const windowTabs = await chrome.tabs.query({ active: true, currentWindow: true })
+    let tab = (windowTabs.length === 1) ? windowTabs[0] : null
+    if (tab !== null) {
+        if (new RegExp(targetUrl).exec(tab.url)) {
+            lastActiveTab = tab
+        }
+    }
+})
+
+chrome.windows.onFocusChanged.addListener(async () => {
+    const windowTabs = await chrome.tabs.query({ active: true, currentWindow: true })
+    let tab = (windowTabs.length === 1) ? windowTabs[0] : null
+    if (tab !== null) {
+        if (new RegExp(targetUrl).exec(tab.url)) {
+            lastActiveTab = tab
+        }
+    }
+})
+
+chrome.tabs.onUpdated.addListener(async (tabid, changeInfo, tab) => {
+    if (new RegExp(targetUrl).exec(tab.url)) {
+        lastActiveTab = tab
+    }
+})
+
+chrome.tabs.onRemoved.addListener(async () => {
     const tabs = await chrome.tabs.query({})
     tabs.forEach(tab => {
-        if (new RegExp('https://animestore.docomo.ne.jp/animestore/sc_d_pc?.*').exec(tab. url)) {
-            chrome.tabs.sendMessage(tab.id, command)
+        if (new RegExp(targetUrl).exec(tab. url)) {
+            lastActiveTab = tab
         }
     })
+})
+
+chrome.commands.onCommand.addListener(async (command) => {
+    chrome.tabs.sendMessage(lastActiveTab.id, command)
 })
--- content_scripts.js
+++ content_scripts.js
@@ -4,27 +4,22 @@ chrome.runtime.onMessage.addListener((request, sender, response) => {
     let button = null
     const isPlaying = (document.querySelector('.play-playing') != null)
 
-    if (request === 'pause') {  // デバイスの一時停止ボタン押下
-        // 再生・一時停止ボタンを押下
+    if (request === 'pause') {
         button = document.querySelector('.playButton')
     }
-    else if (request === 'prev-track') {  // デバイスの巻き戻し（＜）ボタン押下
+    else if (request === 'prev-track') {
         if (isPlaying) {
-            // 動画が再生中なら、30秒戻るボタンを押下
             button = document.querySelector('.backButton')
         }
         else {
-            // 動画が停止中なら、先頭に戻るボタンを押下
             button = document.querySelector('.prevButton')
         }
     }
-    else if (request === 'next-track') {  // デバイスの早送り（＞）ボタン押下
+    else if (request === 'next-track') {
         if (isPlaying) {
-            // 動画が再生中なら、30秒進むボタンを押下
             button = document.querySelector('.skipButton')
         }
         else {
-            // 動画が停止中なら、次の話に進むボタンを押下
             button = document.querySelector('.nextButton')
         }
     }
